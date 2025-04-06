ecda874ac2d1608dab53367f00c40586de4006b9
Kris Kewish <kristopherkewish@gmail.com>
2025-04-06 12:16:25 +1000

    remove refactored functions

--- index.js
+++ index.js
@@ -1,51 +1,7 @@
 console.log("Welcome to Asteroid!");
 
-const fs = require("fs");
-const { parse } = require("csv-parse/sync");
 const { generateGroceryList } = require("./src/services/mealPlanService");
 const { displayGroceryList } = require("./src/utils/displayUtils");
-
-// Function to read and parse CSV file
-function readCSV(filePath) {
-  const fileContent = fs.readFileSync(filePath, "utf-8");
-  return parse(fileContent, {
-    columns: true,
-    skip_empty_lines: true,
-  });
-}
-
-// Function to count meal occurrences
-function countMeals(mealPlan) {
-  const mealCounts = {};
-  mealPlan.forEach((entry) => {
-    const meal = entry.Meal;
-    mealCounts[meal] = (mealCounts[meal] || 0) + 1;
-  });
-  return mealCounts;
-}
-
-// Function to calculate ingredient quantities
-function calculateIngredients(mealCounts, recipeDB) {
-  const ingredientQuantities = {};
-
-  // Process each meal and its ingredients
-  for (const [meal, count] of Object.entries(mealCounts)) {
-    const mealIngredients = recipeDB.filter((recipe) => recipe.Meal === meal);
-
-    mealIngredients.forEach((ingredient) => {
-      const name = ingredient.Ingredients;
-      const amount = parseFloat(ingredient.AmountPerServe) * count;
-
-      if (ingredientQuantities[name]) {
-        ingredientQuantities[name] += amount;
-      } else {
-        ingredientQuantities[name] = amount;
-      }
-    });
-  }
-
-  return ingredientQuantities;
-}
 
 /**
  * Validates and processes command line arguments
